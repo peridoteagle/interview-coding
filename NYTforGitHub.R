c2c644e7232fb44f3674898c26370de290700c84
peridoteagle <32460139+eaglemagic@users.noreply.github.com>
2018-01-05 09:18:48 -0500

    Added code for Shiny app

--- NYTforGitHub.R
+++ NYTforGitHub.R
@@ -234,5 +234,27 @@ corp3 <- VCorpus(VectorSource(docswithheadlines))
 textVector3 <- sapply(corp3, as.character)
 people <- Corpus(VectorSource(textVector3[grep("people", textVector3)]))
 
-#Check Out NYT Shiny App
+#Creating a R-Shiny app
+
+shinyApp(
+  ui = fluidPage(
+    titlePanel("It's All About People"),
+    verbatimTextOutput("text1"),
+    actionButton("button1", "Show Me a Quote!"),
+    verbatimTextOutput("text2")),
+  
+  server = function(input, output, session) {
+    
+    output$text1 <- renderText({
+      session$userData$text1 <- "CEO Quotes that Include the Word 'People'"
+      session$userData$text1})
+    output$text2 <- renderText("Press the button to begin...")
+    
+    observeEvent(input$button1, {
+      llll<-as.character(sample(people,1))
+      llll <- str_replace_all(llll,"people","PEOPLE")
+      output$text2 <- renderText(llll[1])
+    })
+  }
+)
 
